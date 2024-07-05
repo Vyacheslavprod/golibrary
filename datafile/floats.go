6b46514a8a02fad5ccb5faca70150ac2da13dc9c
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-05 14:31:51 +0300

    Add: datafile - slice, average v2 - cmd args

--- datafile/floats.go
+++ datafile/floats.go
@@ -8,27 +8,26 @@ import (
 )
 
 //GetFloats читает значение float64 из каждой строки файла
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
-	i := 0 //Переменная для хранения индекса, по которому должно выполняться присваивание
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64) //Строка прочитанная из файла преобразуется в float
+		number, err := strconv.ParseFloat(scanner.Text(), 64) //Строка прочитанная из файла преобразуется в float
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
-		i++ //Переход к следующему индексу массива
+		numbers = append(numbers, number) //Переход к следующему индексу массива
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil { //Если при сканировании файла произошла ошибка, вернуть ее. строка 19
-		return numbers, err
+		return nil, scanner.Err()
 	}
 
 	return numbers, nil
