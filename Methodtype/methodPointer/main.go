92a2a4f591b9cb11c2fc13da836342f600dde276
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-09 15:57:13 +0300

    Add fuelConversion

--- Methodtype/methodPointer/main.go
+++ Methodtype/methodPointer/main.go
@@ -11,9 +11,12 @@ func (n *Number) Double() {
 	*n *= 2
 }
 
+
+
 func main() {
 	number := Number(4)
 	fmt.Println("Original value of number:", number)
 	number.Double()
 	fmt.Println("number after calling Double:", number)
+	
 }
