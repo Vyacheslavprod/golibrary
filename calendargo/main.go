23242f1c60669cce81f530d43b7d8211ac8ddc18
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-10 15:15:57 +0300

    Add: calendar/event. Update: calendargo, Function standart library

--- calendargo/main.go
+++ calendargo/main.go
@@ -1,4 +1,4 @@
-//Инкапсуляция
+//Инкапсуляция и встраивание
 package main
 
 import (
@@ -25,4 +25,29 @@ func main() {
 	fmt.Println(date.Year())
 	fmt.Println(date.Month())
 	fmt.Println(date.Day())
+
+
+	event := calendar.Event{}
+	err = event.SetTitle("Mom's birthday")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = event.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = event.SetMonth(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = event.SetDay(27)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(event)
+	fmt.Println(event.Title())
+	fmt.Println(event.Year())
+	fmt.Println(event.Month())
+	fmt.Println(event.Day())
+	
 }
