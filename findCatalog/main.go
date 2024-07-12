6939ad533075c43981679cfcb3b2e035656e6953
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-12 16:26:54 +0300

    Modification: findCatalog. 'Вывод каталогов и файлов через рекурсию'

--- findCatalog/main.go
+++ findCatalog/main.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
+//Рекурсивная функция, которая получает путь для обработки
+func scanDirectory(path string) error {
+	fmt.Println(path) //Выводит текущий каталог
+	files, err := os.ReadDir(path) //Получает сегмент с содержимым каталога
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		filepath := filepath.Join(path, file.Name()) //Соединяет путь каталога и имя файла через символ /
+		if file.IsDir() { //Если это каталог...
+			err := scanDirectory(filepath) //..рекурсивно вызывает scanDirectory, но на этот раз с путем подкатолога
+			if err != nil {
+				return nil
+			}
+		} else {
+			fmt.Println(filepath) //Если это обычный файл, просто вывести его имя с путем
+		}
+	}
+	return nil
+}
+
 func main() {
-	files, err := os.ReadDir("my_directory")
+	err := scanDirectory("my_directory") 
 	if err != nil {
 		log.Fatal(err)
-	}
-	
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
-		} else {
-			fmt.Println("File:", file.Name())
-		}
 	}
 }
