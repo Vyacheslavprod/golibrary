5e4a7e5cc5c01efd85e1649500fe7462196e3e50
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-17 15:51:06 +0300

    Add: openfile. v2

---
+++ openfile/openfile.go
@@ -0,0 +1,23 @@
+//Openfile v2
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("aardvark.txt", options, os.FileMode(0060))
+	check(err)
+	_, err = file.Write([]byte("amazing!\n"))
+	check(err)
+	err = file.Close()
+	check(err)
+}
