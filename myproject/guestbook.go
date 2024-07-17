24a5d0de1905a253c4ebef51e63e93ff257fd483
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-17 14:38:40 +0300

    Modified: guestbook.go, new.html. Добавлен метод POST. Guestbook END!

--- myproject/guestbook.go
+++ myproject/guestbook.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
+//GuestBook - структура, используемая при отображении view.html
+type GuestBook struct {
+	SignatureCount int
+	Signatures []string
+}
+
+//chek вызывает log.Fatal для любых ошибок, отличных от nil
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+//viewHandler читает записи гостевой книги и выводит их вместе со счетчиком записей
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html")
@@ -22,8 +31,29 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 		SignatureCount: len(signatures),
 		Signatures: signatures,
 	}
-	err = html.Execute(writer, guestBook)
+	err = html.Execute(writer, guestBook) //Данные структуры GuestBook вставляются в шаблон, а результат записывается в ResponseWriter
 	check(err)
+}
+
+//newHandler отображает форму для ввода записи
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+//createHandler получает запрос POST с добавляемой записью и присоединяет ее к файлу signatures
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature") //Получает значение поля формы
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature) //Добавляет содержимое поля формы в файл
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 }
 
 func getStrings(fileName string) []string {
@@ -45,13 +75,10 @@ func getStrings(fileName string) []string {
 
 }
 
-type GuestBook struct {
-	SignatureCount int
-	Signatures []string
-}
-
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
