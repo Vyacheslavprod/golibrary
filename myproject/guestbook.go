af54bedbc82a9b310d3e7a3c4552e09bc17abd4f
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-17 13:19:55 +0300

    Add myproject/signatures.txt. Modified: view.html, guestbook.go - Вывод шаблона и чтение из файла

--- myproject/guestbook.go
+++ myproject/guestbook.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check(err error) {
@@ -13,10 +15,39 @@ func check(err error) {
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html")
 	check(err)
-	err = html.Execute(writer, nil)
+	guestBook := GuestBook{
+		SignatureCount: len(signatures),
+		Signatures: signatures,
+	}
+	err = html.Execute(writer, guestBook)
 	check(err)
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	//Если возвращается ошибка, указывающая на то, что файл не существует...
+	//...вернуть nil вместо сегмента строк
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+
+}
+
+type GuestBook struct {
+	SignatureCount int
+	Signatures []string
 }
 
 func main() {
