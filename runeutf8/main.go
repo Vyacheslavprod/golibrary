546156291a8fe09c5d939e57ebcb5457ddc680a0
Vyacheslav <vyacheslavproektw@yandex.ru>
2024-07-18 16:00:00 +0300

    Modified: runeutf8

--- runeutf8/main.go
+++ runeutf8/main.go
@@ -7,7 +7,17 @@ import (
 
 func main() {
 	str1 := "ABC"
-	str2 := "АБВ"
+	str2 := "АБВГДЕ"
 	fmt.Println(len(str1))
 	fmt.Println(utf8.RuneCountInString(str2))
+
+	str3 := []rune(str2)
+
+	for i, el := range str1 {
+		fmt.Println(i, string(el))
+	}
+	fmt.Println()
+	for i, el := range str3 {
+		fmt.Println(i, string(el))
+	}
 }
